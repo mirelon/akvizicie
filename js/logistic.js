566247c6afa6f9945e6ff808d06cad1b09501506
kovac <ext-michal.kovac@here.com>
2022-02-11 10:26:38 +0100

    Multiparam logistic regression

--- js/logistic.js
+++ js/logistic.js
@@ -1,31 +1,41 @@
 export function calculateAgeOfAcquisition(data) {
-    return nextLogisticParameters(1000.0, 300.0, data)
+    return nextLogisticParameters(
+        {mean: 1000.0, scale: 1000.0, max: 1.0},
+        1.0,
+        data
+    )
 }
 
-export function logistic(ageOfAcquisition, age) {
-    return 1 / (1 + Math.exp(-(age - ageOfAcquisition) * 0.001))
+// params: object with keys: max, mean, scale
+export function logistic(params, age) {
+    return params.max / (1 + Math.exp(-(age - params.mean) / params.scale))
 }
 
-function error(data, ageOfAcquisition) {
-    const errors = data.map(answer => Math.pow(answer.correct - logistic(ageOfAcquisition, answer.age), 2))
+function error(data, params) {
+    const errors = data.map(answer => Math.pow(answer.correct - logistic(params, answer.age), 2))
     return errors.reduce((a, b) => a + b, 0)
 }
 
-function nextLogisticParameters(ageOfAcquisition, d = 1.0, data) {
-    const currentError = error(data, ageOfAcquisition)
-    console.log(`nextLogisticParameters(${ageOfAcquisition}, ${d}), current error = ${currentError}`)
+function nextLogisticParameters(params, d, data) {
+    const currentError = error(data, params)
+    console.log(`nextLogisticParameters(${JSON.stringify(params, (k, v) => v.toFixed ? Number(v.toFixed(3)) : v)}, ${d}), current error = ${currentError}`)
     const neighbors = [
-        ageOfAcquisition + d,
-        ageOfAcquisition - d
-    ]
-    const newAgeOfAcquisition = neighbors.concat(ageOfAcquisition).reduce((x, y) => error(data, x) < error(data, y) ? x : y)
-    if (newAgeOfAcquisition === ageOfAcquisition) {
+        {...params, mean: params.mean + d * 200.0},
+        {...params, mean: params.mean - d * 200.0},
+        {...params, scale: params.scale + d * 200.0},
+        {...params, scale: params.scale - d * 200.0},
+        {...params, max: params.max + d / 10.0},
+        {...params, max: params.max - d / 10.0}]
+        .filter(neighbor => neighbor.scale > neighbor.mean / 100.0)
+    const newParams = neighbors.concat(params)
+        .reduce((x, y) => error(data, x) < error(data, y) ? x : y)
+    if (newParams === params) {
         if (d > 0.1) {
-            return nextLogisticParameters(ageOfAcquisition, d / 2, data)
+            return nextLogisticParameters(params, d / 2, data)
         } else {
-            return ageOfAcquisition
+            return params
         }
     } else {
-        return nextLogisticParameters(newAgeOfAcquisition, d, data)
+        return nextLogisticParameters(newParams, d, data)
     }
 }
